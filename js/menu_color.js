8449ac13a4a9f769c08dd25f9199b11af2a00b62
Chepurina <galina.chepurina@gmail.com>
2016-04-07 23:15:11 +0300

    plugins jquery

---
+++ js/menu_color.js
@@ -0,0 +1,13 @@
+ $(function(){  
+ 	$('nav li .dropdown').hover(function() { 
+ 	$(this).stop().animate({  
+ 		backgroundColor: "#99CCFF" 
+ 	}, 400); 
+  },function() { 
+ 	$(this).stop().animate({  
+ 		backgroundColor: '#99CCFF'  
+ 	}, 400); 
+  }); 
+ }); 
+
+ 
---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict'
+$(function() {
+    $('.jcarousel').jcarousel({
+        wrap: 'circular'
+    });
+});
+
